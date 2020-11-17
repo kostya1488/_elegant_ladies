1f15e6b689b6d0563bac487ab4c7d7fefbbf8a1f
kostya <matvienkoigor956@gmail.com>
2020-11-17 18:47:10 +0200

    100vh

--- js/script.js
+++ js/script.js
@@ -1,12 +1,24 @@
 $(document).ready(function() {
-    if ($('body').hasClass('mobile')) {
-        //добавляем inline стиль, в котором фиксируем высоту титульного блока
-        $('.main').css({ height: window.innerHeight });
-    }
+    // if ($('body').hasClass('mobile')) {
+    //     //добавляем inline стиль, в котором фиксируем высоту титульного блока
+    //     $('.main').css({ height: window.innerHeight });
+    // }
 
     $('.menu_toggle').click(function(e) {
         e.preventDefault();
         $('.menu_area').toggleClass('activated');
     });
 
+    let h = window.innerHeight
+    $('.swiper-container').css('height', h)
+
 });
+
+// let vh = window.innerHeight * 0.01;
+// document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// window.addEventListener('resize', () => {
+//     // We execute the same script as before
+//     let vh = window.innerHeight * 0.01;
+//     document.documentElement.style.setProperty('--vh', `${vh}px`);
+// });
